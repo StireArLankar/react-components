c8643427617007f9207227e681d211e50deead61
Stire <stire.ru@gmail.com>
2020-01-18 01:01:14 +0300

    Added Cancel

---
+++ src/components/gesture/Cancel/Cancel.story.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { withCenteredStyle } from '../../../_storybook/withCenteredStyle'
+import { withCustomTheme } from '../../../_storybook/withCustomTheme'
+
+import { Cancel } from '.'
+
+export default {
+  title: 'Gesture|Cancel',
+  decorators: [withCenteredStyle({ width: '100%' }), withCustomTheme],
+}
+
+export const cancel = () => <Cancel />
---
+++ src/components/gesture/Cancel/index.tsx
@@ -0,0 +1,44 @@
+import React, { useRef } from 'react'
+import { animated, useSpring } from 'react-spring'
+import { useGesture } from 'react-use-gesture'
+import useStyles from './useStyles'
+
+export const Cancel = () => {
+  const classes = useStyles()
+
+  // const [check, setCheck] = useState(false)
+
+  const check = useRef(false)
+
+  const [{ x, backgroundColor }, set] = useSpring(() => ({
+    x: 0,
+    backgroundColor: '#91c9f9',
+  }))
+
+  const bind = useGesture({
+    onDrag: ({ down, movement: [mx], cancel }) => {
+      set({
+        x: down ? mx : 0,
+        immediate: down,
+        backgroundColor: check.current ? '#ffb6c1' : '#91c9f9',
+      })
+
+      if (Math.abs(mx) > 100) {
+        cancel && cancel()
+        check.current = true
+      }
+    },
+    onDragStart: () => (check.current = false),
+  })
+
+  return (
+    <animated.div
+      className={classes.box}
+      {...bind()}
+      style={{
+        transform: x.interpolate((val) => `translate3d(${val}px, 0, 0)`),
+        backgroundColor,
+      }}
+    />
+  )
+}
