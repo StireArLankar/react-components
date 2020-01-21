a3f2c58f4ae821f4c86bad599fb33362600223cd
Stire <stire.ru@gmail.com>
2020-01-21 23:13:14 +0300

    Copied multicard

---
+++ src/components/gesture/MultiCard/index.tsx
@@ -0,0 +1,69 @@
+import React, { useRef, useState, useEffect } from 'react'
+import { useSpring, animated, interpolate } from 'react-spring'
+import { useGesture } from 'react-use-gesture'
+import imgs from './imgs'
+import useStyles from './useStyles'
+import clsx from 'clsx'
+
+const wheel = (y: number) => {
+  const imgHeight = window.innerWidth * 0.2 - 20
+  return `translateY(${-imgHeight * (y < 0 ? 6 : 1) - (y % (imgHeight * 5))}px`
+}
+
+document.addEventListener('gesturestart', (e) => e.preventDefault())
+document.addEventListener('gesturechange', (e) => e.preventDefault())
+
+export const MultiCard = () => {
+  const classes = useStyles()
+
+  const domTarget = useRef(null)
+
+  const [{ x, y, scale }, set] = useSpring(() => ({
+    scale: 1,
+    x: 0,
+    y: 0,
+    config: { mass: 5, tension: 350, friction: 40 },
+  }))
+
+  const [{ wheelY }, setWheel] = useSpring(() => ({ wheelY: 0 }))
+  const [drag, setDrag] = useState(false)
+
+  const bind = useGesture(
+    {
+      onDragStart: () => setDrag(true),
+      onDrag: ({ offset: [x, y] }) => set({ x, y, scale: 1 }),
+      onDragEnd: () => setDrag(false),
+      onHover: ({ hovering }) => !hovering && set({ scale: 1 }),
+      onWheel: ({ offset: [, y] }) => setWheel({ wheelY: y }),
+    },
+    { domTarget, event: { passive: false } }
+  )
+
+  useEffect(bind, [bind])
+
+  return (
+    <animated.div
+      ref={domTarget}
+      className={clsx(classes.card, drag && 'dragging')}
+      style={{
+        transform: interpolate(
+          [x, y, scale as any],
+          (x, y, s) =>
+            `perspective(600px) translate3d(${x}px, ${y}px, 0) scale(${s})`
+        ),
+        // x,
+        // y,
+        // scale: interpolate([scale as any, zoom as any] as any, (s, z) => s + z),
+      }}
+    >
+      <animated.div
+        className={classes.inner}
+        style={{ transform: wheelY.interpolate(wheel) }}
+      >
+        {imgs.map((img, i) => (
+          <div key={i} style={{ backgroundImage: `url(${img})` }} />
+        ))}
+      </animated.div>
+    </animated.div>
+  )
+}
---
+++ src/components/gesture/MultiCard/useStyles.ts
@@ -0,0 +1,45 @@
+import createStyles from '@material-ui/core/styles/createStyles'
+import makeStyles from '@material-ui/core/styles/makeStyles'
+import { Theme } from '@material-ui/core/styles/createMuiTheme'
+
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    card: {
+      position: 'relative',
+      width: '20vw',
+      height: '20vw',
+      background: 'grey',
+      borderRadius: 5,
+      boxShadow: '0px 10px 30px -5px rgba(0, 0, 0, 0.3)',
+      transition: 'box-shadow 0.5s, opacity 0.5s',
+      willChange: 'transform',
+      border: '10px solid white',
+      cursor: 'grab',
+      overflow: 'hidden',
+
+      '&.dragging': {
+        cursor: 'grabbing',
+      },
+      '&.disabled': {
+        opacity: 0.5,
+        cursor: 'default',
+      },
+      '&:hover': {
+        boxShadow: '0px 30px 100px -10px rgba(0, 0, 0, 0.4)',
+      },
+    },
+    inner: {
+      willChange: 'transform',
+      height: '100%',
+      margin: '0vw 0',
+      '& > *': {
+        height: '100%',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        margin: '0vw 0',
+      },
+    },
+  })
+)
+
+export default useStyles
