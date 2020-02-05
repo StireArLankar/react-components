b08d582edb27f274853e46fadb3b7527f0664101
Stire <stire.ru@gmail.com>
2020-02-06 00:46:58 +0300

    ScrollBar: second commit (onDrag + onWheel)

--- src/components/gesture/ScrollBar/ScrollBar.tsx
+++ src/components/gesture/ScrollBar/ScrollBar.tsx
@@ -1,13 +1,18 @@
-import React, { PropsWithChildren, useState, useEffect } from 'react'
+import React, { PropsWithChildren, useState, useEffect, Fragment } from 'react'
 import { useSpring, animated } from 'react-spring'
 import { useGesture } from 'react-use-gesture'
 
 import { useStyles } from './useStyles'
 import useMeasure from 'react-use-measure'
 
-const getBounds = (content: number, container: number, val: number) => ({
-  bottom: content <= container ? 100 : content - container - val,
-  top: content <= container ? -100 : -val,
+const getWheelBounds = (content: number, container: number, val: number) => ({
+  bottom: content <= container ? 0 : content - container - val,
+  top: content <= container ? 0 : -val,
+})
+
+const getDragBounds = (content: number, container: number, val: number) => ({
+  top: content <= container ? 0 : -content + container + val,
+  bottom: content <= container ? 0 : val,
 })
 
 export const ScrollBar = (props: PropsWithChildren<{}>) => {
@@ -29,42 +34,55 @@ export const ScrollBar = (props: PropsWithChildren<{}>) => {
     set({ y: val })
   }, [val, set, acting])
 
-  const bounds = getBounds(contentHeight, containerHeight, val)
+  const handler = (y: number, check: boolean) => {
+    if (check) {
+      set({ y: val + y })
+    } else {
+      setVal((prev) => prev + y)
+    }
+  }
 
   const bind = useGesture(
     {
-      // onDrag: ({ movement: [, y], down }) => {
-      //   if (down) {
-      //     set({ y: val + y })
-      //   } else {
-      //     setVal((prev) => prev + y)
-      //   }
-      // },
-      onWheel: ({ movement: [, y], wheeling }) => {
-        if (wheeling) {
-          setActing(true)
-          set({ y: val + y })
-        } else {
-          setActing(false)
-          setVal((prev) => prev + y)
-        }
-      },
+      onDrag: ({ movement: [, y], down }) => handler(-y, down),
+      onWheel: ({ movement: [, y], wheeling }) => handler(y, wheeling),
+      onWheelEnd: () => setActing(false),
+      onWheelStart: () => setActing(true),
+      onDragEnd: () => setActing(false),
+      onDragStart: () => setActing(true),
     },
     {
-      eventOptions: { passive: true },
-      // drag: {
-      //   bounds: getBounds(contentHeight, containerHeight, val),
-      //   rubberband: 0.2,
-      //   filterTaps: true,
-      // },
       wheel: {
-        rubberband: 0.2,
-        bounds,
+        bounds: getWheelBounds(contentHeight, containerHeight, val),
+        rubberband: 0.1,
+      },
+      drag: {
+        rubberband: height > 0 ? 0.3 : 0.15,
+        bounds: getDragBounds(contentHeight, containerHeight, val),
+        filterTaps: true,
       },
     }
   )
 
-  const renderScrollBar = () => <animated.div className={classes.progress} />
+  const renderBlock = () => (
+    <div style={{ height: 100, width: 100, background: 'red' }} />
+  )
+
+  const renderSpacer = () => (
+    <div style={{ height: 100, margin: '20px 0', width: 100 }} />
+  )
+
+  const renderItems = (amount: number) => (
+    <Fragment>
+      {renderBlock()}
+      {Array.from({ length: amount - 1 }, () => '').map((_, index) => (
+        <Fragment key={index}>
+          {renderSpacer()}
+          {renderBlock()}
+        </Fragment>
+      ))}
+    </Fragment>
+  )
 
   return (
     <div className={classes.container} {...bind()} ref={containerRef}>
@@ -76,15 +94,8 @@ export const ScrollBar = (props: PropsWithChildren<{}>) => {
         }}
       >
         {children}
-        <div style={{ height: 100, width: 100, background: 'red' }} />
-        <div style={{ height: 100, margin: '20px 0', width: 100 }} />
-        <div style={{ height: 100, width: 100, background: 'red' }} />
-        <div style={{ height: 100, margin: '20px 0', width: 100 }} />
-        <div style={{ height: 100, width: 100, background: 'red' }} />
-        <div style={{ height: 100, margin: '20px 0', width: 100 }} />
-        <div style={{ height: 100, width: 100, background: 'red' }} />
+        {!children && renderItems(5)}
       </animated.div>
-      {height > 0 && renderScrollBar()}
     </div>
   )
 }
