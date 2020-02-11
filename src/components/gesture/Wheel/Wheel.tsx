9e6192f3a4bc5b4d66639bd537d808887e376217
Stire <stire.ru@gmail.com>
2020-02-12 00:27:44 +0300

    Added background for wheel

--- src/components/gesture/Wheel/Wheel.tsx
+++ src/components/gesture/Wheel/Wheel.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { useSpring, animated } from 'react-spring'
 import { useDrag } from 'react-use-gesture'
+import clamp from 'lodash-es/clamp'
 
 import useStyles from './useStyles'
 
 import path from './path'
+import useMeasure from 'react-use-measure'
 
 const constrains = (val: number, max: number) => {
   return val > 0 ? Math.min(val, max) : Math.max(val, -max)
 }
 
-const getMax = () => window.innerWidth / 2 - 200
-
-const circleQuarter = (Math.PI * 120) / 2
+// const circleQuarter = (Math.PI * 120) / 2
 
 const transX = (x: number) => `translate3d(${x}px, 0, 0)`
 
@@ -21,26 +21,43 @@ const alpha = 180 / (120 * Math.PI)
 
 const rotZ = (x: number) => `rotateZ(${alpha * x}deg)`
 
+const scaleY = (x: number, i: number, width: number, amount: number) => {
+  const wid = width / amount
+  // [-w/2 ... + w/2]
+  const positive = x + width / 2
+  // [0 .... w]
+  const itemValue = positive - i * wid
+  // [0 ... w / n]
+
+  const val = clamp(itemValue / wid, 0, 1)
+
+  // return `scale3d(1, ${val}, 1)`
+  return `translate3d(0, ${(1 - val) * 100}%, 0)`
+}
+
 export const Wheel = () => {
   const classes = useStyles()
 
+  const [ref, { width }] = useMeasure({
+    debounce: 100,
+  })
   const [{ x }, set] = useSpring(() => ({ x: 0 }))
 
   const bind = useDrag(
-    ({ down, movement: [x], vxvy: [vx], tap }) => {
+    ({ down, movement: [x], vxvy: [vx], tap, elapsedTime }) => {
       if (!down && tap) {
-        set({ x: x + circleQuarter })
-        return
+        // set({ x: constrains(x + circleQuarter, width / 2) })
+        return set({ x: constrains(x + elapsedTime, width / 2) })
       }
       set({
-        x: down ? x : constrains(x + vx * 100, getMax()),
+        x: down ? x : constrains(x + vx * 100, width / 2),
       })
     },
     {
       initial: () => [x.getValue(), 0],
       bounds: {
-        right: getMax(),
-        left: -getMax(),
+        right: width / 2,
+        left: -width / 2,
       },
       rubberband: 0.4,
       axis: 'x',
@@ -48,8 +65,20 @@ export const Wheel = () => {
     }
   )
 
+  const renderItems = () =>
+    Array.from({ length: Math.floor(width / 40) }, (_, i) => (
+      <animated.li
+        className={classes.item}
+        style={{
+          transform: x.interpolate((x) =>
+            scaleY(x, i, width, Math.floor(width / 40))
+          ),
+        }}
+      />
+    ))
+
   return (
-    <div>
+    <Fragment>
       <animated.div
         className={classes.box}
         {...bind()}
@@ -67,6 +96,9 @@ export const Wheel = () => {
           <path fill='currentColor' d={path} />
         </animated.svg>
       </animated.div>
-    </div>
+      <ul className={classes.list} ref={ref}>
+        {renderItems()}
+      </ul>
+    </Fragment>
   )
 }
--- src/components/gesture/Wheel/useStyles.ts
+++ src/components/gesture/Wheel/useStyles.ts
@@ -16,6 +16,43 @@ export const useStyles = makeStyles((theme: Theme) =>
       height: '100%',
       color: theme.custom.border,
     },
+    list: {
+      position: 'absolute',
+      padding: 0,
+      margin: '0 auto',
+      display: 'flex',
+      justifyContent: 'space-around',
+      height: '80vh',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      width: `calc(100% - 300px)`,
+      listStyle: 'none',
+      overflow: 'hidden',
+    },
+    item: {
+      height: '100%',
+      width: 20,
+      transformOrigin: 'bottom',
+      borderTopLeftRadius: 10,
+      borderTopRightRadius: 10,
+      opacity: 0.8,
+      '&:nth-child(5n)': {
+        background: 'linear-gradient(180deg, #f093fb 0%, #f5576c 100%)',
+      },
+      '&:nth-child(5n + 1)': {
+        background: 'linear-gradient(180deg, #a8edea 0%, #fed6e3 100%)',
+      },
+      '&:nth-child(5n + 2)': {
+        background: 'linear-gradient(180deg, #d299c2 0%, #fef9d7 100%)',
+      },
+      '&:nth-child(5n + 3)': {
+        background: 'linear-gradient(180deg, #ebc0fd 0%, #d9ded8 100%)',
+      },
+      '&:nth-child(5n + 4)': {
+        background: 'linear-gradient(180deg, #f6d365 0%, #fda085 100%)',
+      },
+    },
   })
 )
 
