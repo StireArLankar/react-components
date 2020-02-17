70b78866fb66277547c24f76a4b110810fb5d768
Stire <stire.ru@gmail.com>
2020-02-18 00:17:10 +0300

    Heart animation

---
+++ src/components/design/Heart/Heart.story.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { withCustomTheme } from '../../../_storybook/withCustomTheme'
+
+import { withCenteredStyle } from '../../../_storybook/withCenteredStyle'
+import { Heart } from './Heart'
+
+export default {
+  title: 'Design|Heart',
+  decorators: [withCustomTheme, withCenteredStyle({ width: '100%' })],
+}
+
+export const heart = () => <Heart />
---
+++ src/components/design/Heart/Heart.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import clsx from 'clsx'
+
+import HeartPng from './web-heart-animation.png'
+import useStyles from './useStyles'
+
+export const Heart = () => {
+  const classes = useStyles()
+
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen((prev) => !prev)
+
+  const heartClass = clsx({
+    [classes.heart]: true,
+    [classes.animation]: isOpen,
+  })
+
+  return (
+    <div className={classes.container} onClick={toggle}>
+      <div
+        className={heartClass}
+        style={{
+          backgroundImage: `url(${HeartPng}`,
+        }}
+      ></div>
+    </div>
+  )
+}
