75e5b04e5901d23990bbba3b5d31cba887d04035
Stire <stire.ru@gmail.com>
2020-03-28 01:37:36 +0300

    Ripple

---
+++ src/components/design/Ripple/_.story.tsx
@@ -0,0 +1,39 @@
+import React, { Fragment } from 'react'
+import { text, boolean } from '@storybook/addon-knobs'
+
+import { withCenteredStyle } from '../../../_storybook/withCenteredStyle'
+import { withCustomTheme } from '../../../_storybook/withCustomTheme'
+import { withTopLabel } from '../../../_storybook/withTopLabel'
+import { themeColors } from '../../../theme/theme.styles'
+
+import { Ripple, RippleProps } from '.'
+
+const label = (
+  <Fragment>
+    <span>Credits to </span>
+    <a
+      href='https://www.youtube.com/watch?v=ueyRcYEmsrI'
+      style={{ color: themeColors.darkBlue, textDecoration: 'none' }}
+    >
+      video
+    </a>
+  </Fragment>
+)
+
+export default {
+  title: 'Design|Ripple',
+  decorators: [
+    withCustomTheme,
+    withCenteredStyle({ width: '100%' }),
+    withTopLabel(label),
+  ],
+}
+
+export const button = () => {
+  const props: RippleProps = {
+    children: text('text', 'Hello world'),
+    secondary: boolean('secondary', false),
+  }
+
+  return <Ripple {...props} />
+}
---
+++ src/components/design/Ripple/index.tsx
@@ -0,0 +1,48 @@
+import clsx from 'clsx'
+import React, { PropsWithChildren, useState } from 'react'
+
+import useStyles from './Ripple.styles'
+
+export interface RippleProps
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  secondary?: boolean
+}
+
+export const Ripple = (props: PropsWithChildren<RippleProps>) => {
+  const { children, onClick, secondary, className, ...rest } = props
+
+  const classes = useStyles()
+
+  const [ripples, setRipples] = useState<[number, number, number][]>([])
+
+  const onButtonClick = (e: any) => {
+    onClick && onClick(e)
+    const x = e.clientX - e.target.offsetLeft
+    const y = e.clientY - e.target.offsetTop
+
+    setRipples((prev) => [...prev, [x, y, Date.now()]])
+    setTimeout(() => setRipples((prev) => prev.slice(1)), 1000)
+  }
+
+  const buttonClass = clsx(
+    classes.button,
+    secondary && classes.secondary,
+    className
+  )
+
+  return (
+    <button {...rest} onClick={onButtonClick} className={buttonClass}>
+      {ripples.map((ripple) => (
+        <span
+          key={ripple[2]}
+          className={classes.ripple}
+          style={{ left: ripple[0], top: ripple[1] }}
+        />
+      ))}
+      {children}
+    </button>
+  )
+}
