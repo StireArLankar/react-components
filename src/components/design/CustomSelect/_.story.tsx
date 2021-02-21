2c89e998c3e054d8b159bceec42f30b528d13ef1
Stire <stire.ru@gmail.com>
2021-02-21 22:54:23 +0300

    added custom select

---
+++ src/components/design/CustomSelect/_.story.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+
+import { withCustomTheme } from '../../../_storybook/withCustomTheme'
+import { withCenteredStyle } from '../../../_storybook/withCenteredStyle'
+
+import Temp from '.'
+
+export default {
+  title: 'Design|CustomSelect',
+  decorators: [
+    withCustomTheme,
+    withCenteredStyle({
+      width: '100%',
+      background: 'teal',
+      minHeight: `160vh`,
+      display: 'grid',
+      placeItems: 'center',
+    }),
+  ],
+}
+
+const options = [
+  {
+    label: 'Option 1',
+    value: '1',
+  },
+  {
+    label: 'Option 2',
+    value: '2',
+  },
+  {
+    label: 'Option 3',
+    value: '3',
+  },
+  {
+    label: 'Option 4',
+    value: '4',
+  },
+  {
+    label: 'Option 5',
+    value: '5',
+  },
+]
+
+const Controlled = () => {
+  const [value, setValue] = useState('1')
+
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3, auto)',
+        gridGap: 10,
+        alignItems: 'center',
+      }}
+    >
+      <a href='#11'>for accessible</a>
+      <Temp value={value} onChange={setValue} items={options} />
+
+      <a href='#11'>for accessible</a>
+
+      <div
+        style={{
+          position: 'fixed',
+          bottom: 0,
+          margin: '0 auto',
+          left: '50%',
+          transform: 'translate(-50%)',
+        }}
+      >
+        <Temp value={value} onChange={setValue} items={options} />
+      </div>
+    </div>
+  )
+}
+
+export const example = () => <Controlled />
---
+++ src/components/design/CustomSelect/index.tsx
@@ -0,0 +1,148 @@
+import { useLayer } from 'react-laag'
+import { useSelect } from 'downshift'
+import { motion, AnimatePresence, Variants } from 'framer-motion'
+import React, { useMemo, useRef } from 'react'
+import clsx from 'clsx'
+
+import useStyles from './styles'
+
+export interface SelectProps {
+  value: string
+  items: { value: string; label: string }[]
+  onChange: (val: string) => void
+}
+
+const variants: Variants = {
+  initial: {
+    opacity: 0,
+    height: 0,
+    overflowY: 'hidden',
+  },
+  animate: {
+    opacity: 1,
+    height: 'auto',
+    transitionEnd: {
+      ['overflowY' as any]: 'auto',
+    },
+  },
+  exit: { opacity: 0, height: 0, overflowY: 'hidden' },
+}
+
+const variantsList: Variants = {
+  initial: {},
+  animate: {},
+  exit: {},
+}
+
+const itemToString = (item: { value: string; label: string } | null) =>
+  item ? item.value : ''
+
+const getLabel = (item: { value: string; label: string } | null) =>
+  item ? item.label : ''
+
+export const Select = (props: SelectProps) => {
+  const classes = useStyles()
+  const { items, onChange, value } = props
+  // This is the place to store the fruits that match the input value
+
+  const selected = useMemo(() => {
+    return items.find((item) => item.value === value)
+  }, [items, value])
+
+  const {
+    isOpen,
+    selectedItem,
+    getToggleButtonProps,
+    getMenuProps,
+    getItemProps,
+    highlightedIndex,
+  } = useSelect({
+    // isOpen: true,
+    items: items,
+    circularNavigation: true,
+    itemToString: itemToString,
+    selectedItem: selected,
+    onSelectedItemChange: (val) =>
+      val.selectedItem?.value && onChange(val.selectedItem?.value),
+  })
+
+  const showMenu = isOpen
+
+  const ref = useRef<HTMLDivElement>(null)
+
+  const {
+    renderLayer,
+    triggerProps,
+    layerProps,
+    layerSide,
+    triggerBounds,
+  } = useLayer({
+    // container: ref.current ?? undefined,
+    isOpen: showMenu,
+    overflowContainer: true,
+    placement: 'bottom-center',
+    auto: true,
+    triggerOffset: 9,
+  })
+
+  return (
+    <div ref={ref} style={{ position: 'relative' }}>
+      <button
+        type='button'
+        {...getToggleButtonProps(triggerProps)}
+        className={classes.label}
+      >
+        <span>{getLabel(selectedItem) || '--'}</span>
+        <svg width='16' height='16' viewBox='0 0 16 16'>
+          <path
+            d='M3.52864 6.02861C3.78899 5.76826 4.2111 5.76826 4.47145 6.02861L8.00004 9.5572L11.5286 6.02861C11.789 5.76826 12.2111 5.76826 12.4714 6.02861C12.7318 6.28896 12.7318 6.71107 12.4714 6.97141L8.47145 10.9714C8.2111 11.2318 7.78899 11.2318 7.52864 10.9714L3.52864 6.97141C3.26829 6.71107 3.26829 6.28896 3.52864 6.02861Z'
+            fill='currentColor'
+          />
+        </svg>
+      </button>
+      <div {...getMenuProps()}>
+        {renderLayer(
+          <AnimatePresence>
+            {showMenu && (
+              <motion.div
+                ref={layerProps.ref}
+                className={classes.wrapper}
+                variants={variants}
+                initial='initial'
+                animate='animate'
+                exit='exit'
+                style={{
+                  ...layerProps.style,
+                  width: triggerBounds?.width,
+                  originY: layerSide === 'top' ? '100%' : 0,
+                }}
+                transition={{ duration: 0.2 }}
+              >
+                <motion.ul
+                  variants={variantsList}
+                  transition={{ duration: 0.2 }}
+                  className={classes.list}
+                >
+                  {items.map((item, index) => (
+                    <li
+                      key={`${item}${index}`}
+                      className={clsx(
+                        classes.item,
+                        highlightedIndex === index && classes.active
+                      )}
+                      {...getItemProps({ item, index })}
+                    >
+                      {item.label}
+                    </li>
+                  ))}
+                </motion.ul>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Select
