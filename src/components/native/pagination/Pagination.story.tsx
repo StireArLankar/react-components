f3c077c9bae0fb78f95795ff124bdd1273946bcd
Stire <stire.ru@gmail.com>
2023-02-18 10:41:08 +0700

    added old pagination to storybook

--- src/components/native/pagination/Pagination.story.tsx
+++ src/components/native/pagination/Pagination.story.tsx
@@ -1,14 +1,54 @@
+import { useState } from 'react'
+
+import { action } from '@storybook/addon-actions'
+import { Meta, StoryObj } from '@storybook/react'
+
 import { withCenteredStyle } from '~/_storybook/withCenteredStyle'
 import { withCustomTheme } from '~/_storybook/withCustomTheme'
 
-export default {
+import Component from './Pagination'
+
+const meta = {
   title: 'Native/Pagination',
+  component: Component,
   decorators: [
-    withCenteredStyle({ width: '100%', padding: 20 }),
+    withCenteredStyle({ width: '100%', padding: 20, display: 'block' }),
     withCustomTheme,
   ],
+} satisfies Meta<typeof Component>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+interface ControlledProps {
+  totalPages: number
+  itemsPerPage: number
 }
 
-export const Example = () => (
-  <div style={{ color: 'white', fontSize: 30 }}>BROKEN IN STORYBOOK</div>
-)
+const Controlled = (props: ControlledProps) => {
+  const [page, setPage] = useState(1)
+
+  return (
+    <Component
+      itemsPerPage={props.itemsPerPage}
+      currentPage={page}
+      itemsCount={props.totalPages * props.itemsPerPage}
+      setPage={setPage}
+    />
+  )
+}
+
+export const Example: StoryObj<typeof Controlled> = {
+  render: (args) => <Controlled {...args} />,
+  args: { itemsPerPage: 10, totalPages: 10 },
+}
+
+export const Uncontrolled: Story = {
+  args: {
+    itemsPerPage: 10,
+    currentPage: 0,
+    itemsCount: 100,
+    setPage: action('setPage'),
+  },
+}
--- src/components/native/pagination/Pagination.tsx
+++ src/components/native/pagination/Pagination.tsx
@@ -1,12 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
-// import React, { useEffect, MouseEvent } from 'react'
-// import { useLocation, useHistory } from 'react-router-dom'
+import { MouseEvent } from 'react'
 
-// import clsx from 'clsx'
-// import queryString from 'query-string'
+import clsx from 'clsx'
 
-// import style from './pagination.module.scss'
+import style from './pagination.module.scss'
 
 interface PaginationProps {
   itemsCount: number
@@ -23,143 +21,141 @@ interface PaginationProps {
 //   return href
 // }
 
-// const Pagination = (props: PaginationProps) => {
-//   const { itemsCount, itemsPerPage, currentPage, setPage } = props
-//   const totalPages = Math.ceil(itemsCount / itemsPerPage)
+const Pagination = (props: PaginationProps) => {
+  const { itemsCount, itemsPerPage, currentPage, setPage } = props
+  const totalPages = Math.ceil(itemsCount / itemsPerPage)
 
-//   const { search } = useLocation()
-//   const history = useHistory()
+  // const { search } = useLocation()
+  // const history = useHistory()
 
-//   useEffect(() => {
-//     const { query } = queryString.parseUrl(search)
-//     if (query.page && currentPage !== +query.page) {
-//       setPage(+query.page)
-//     }
-//   }, [currentPage, search, setPage])
+  // useEffect(() => {
+  //   const { query } = queryString.parseUrl(search)
+  //   if (query.page && currentPage !== +query.page) {
+  //     setPage(+query.page)
+  //   }
+  // }, [currentPage, search, setPage])
 
-//   const pushNewHistory = (number: number) => {
-//     const href = updateQuery(number, search)
-//     history.push(href)
-//   }
+  // const pushNewHistory = (number: number) => {
+  //   const href = updateQuery(number, search)
+  //   history.push(href)
+  // }
 
-//   useEffect(() => {
-//     const href = updateQuery(currentPage, search)
-//     history.replace(href)
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, []) //FIXME
+  // useEffect(() => {
+  //   const href = updateQuery(currentPage, search)
+  //   history.replace(href)
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []) //FIXME
 
-//   const renderTitle = () =>
-//     `Pagination with page query and pushing new instances to history`
+  const renderTitle = () =>
+    `Pagination with page query and pushing new instances to history`
 
-//   const renderNumberButtons = () => {
-//     let arr = Array.from({ length: totalPages }, (_, index) =>
-//       String(index + 1)
-//     )
-//     if (arr.length > 7) {
-//       if (currentPage <= 5) {
-//         arr = [...arr.slice(0, 7), '...', ...arr.slice(-1)]
-//       } else if (currentPage >= totalPages - 4) {
-//         arr = [...arr.slice(0, 1), '...', ...arr.slice(-7)]
-//       } else {
-//         arr = [
-//           ...arr.slice(0, 1),
-//           '...',
-//           ...arr.slice(currentPage - 3, currentPage + 2),
-//           '...',
-//           ...arr.slice(-1),
-//         ]
-//       }
-//     }
+  const renderNumberButtons = () => {
+    let arr = Array.from({ length: totalPages }, (_, index) =>
+      String(index + 1)
+    )
+    if (arr.length > 7) {
+      if (currentPage <= 5) {
+        arr = [...arr.slice(0, 7), '...', ...arr.slice(-1)]
+      } else if (currentPage >= totalPages - 4) {
+        arr = [...arr.slice(0, 1), '...', ...arr.slice(-7)]
+      } else {
+        arr = [
+          ...arr.slice(0, 1),
+          '...',
+          ...arr.slice(currentPage - 3, currentPage + 2),
+          '...',
+          ...arr.slice(-1),
+        ]
+      }
+    }
 
-//     return arr.map((page, index) => {
-//       const cName = clsx({
-//         [style.item]: true,
-//         [style.page]: true,
-//         [style.active]: page === String(currentPage),
-//         [style.disabled]: page === '...',
-//       })
+    return arr.map((page, index) => {
+      const cName = clsx({
+        [style.item]: true,
+        [style.page]: true,
+        [style.active]: page === String(currentPage),
+        [style.disabled]: page === '...',
+      })
 
-//       return (
-//         <li className={cName} onClick={onNumberClick(page)} key={index}>
-//           <button className={style.link}>{page}</button>
-//         </li>
-//       )
-//     })
-//   }
+      return (
+        <li className={cName} onClick={onNumberClick(page)} key={index}>
+          <button className={style.link}>{page}</button>
+        </li>
+      )
+    })
+  }
 
-//   const onNumberClick = (number: string) => (e: MouseEvent) => {
-//     e.preventDefault()
-//     const num = Number(number)
-//     if (!isNaN(num)) {
-//       setPage(num)
-//       pushNewHistory(num)
-//     }
-//   }
+  const onNumberClick = (number: string) => (e: MouseEvent) => {
+    e.preventDefault()
+    const num = Number(number)
+    if (!isNaN(num)) {
+      setPage(num)
+      // pushNewHistory(num)
+    }
+  }
 
-//   const onNextClick = (e: MouseEvent) => {
-//     e.preventDefault()
-//     if (currentPage < totalPages) {
-//       setPage(currentPage + 1)
-//       pushNewHistory(currentPage + 1)
-//     }
-//   }
+  const onNextClick = (e: MouseEvent) => {
+    e.preventDefault()
+    if (currentPage < totalPages) {
+      setPage(currentPage + 1)
+      // pushNewHistory(currentPage + 1)
+    }
+  }
 
-//   const renderNextButton = () => {
-//     const cName = clsx({
-//       [style.item]: true,
-//       [style.next]: true,
-//       [style.disabled]: currentPage === totalPages,
-//     })
+  const renderNextButton = () => {
+    const cName = clsx({
+      [style.item]: true,
+      [style.next]: true,
+      [style.disabled]: currentPage === totalPages,
+    })
 
-//     return (
-//       <li className={cName}>
-//         <button onClick={onNextClick} className={style.link}>
-//           Next
-//         </button>
-//       </li>
-//     )
-//   }
+    return (
+      <li className={cName}>
+        <button onClick={onNextClick} className={style.link}>
+          Next
+        </button>
+      </li>
+    )
+  }
 
-//   const onPrevClick = (e: MouseEvent) => {
-//     e.preventDefault()
-//     if (currentPage > 1) {
-//       setPage(currentPage - 1)
-//       pushNewHistory(currentPage - 1)
-//     }
-//   }
+  const onPrevClick = (e: MouseEvent) => {
+    e.preventDefault()
+    if (currentPage > 1) {
+      setPage(currentPage - 1)
+      // pushNewHistory(currentPage - 1)
+    }
+  }
 
-//   const renderPrevButton = () => {
-//     const cName = clsx({
-//       [style.item]: true,
-//       [style.prev]: true,
-//       [style.disabled]: currentPage === 1,
-//     })
+  const renderPrevButton = () => {
+    const cName = clsx({
+      [style.item]: true,
+      [style.prev]: true,
+      [style.disabled]: currentPage === 1,
+    })
 
-//     return (
-//       <li className={cName}>
-//         <button onClick={onPrevClick} className={style.link}>
-//           Prev
-//         </button>
-//       </li>
-//     )
-//   }
+    return (
+      <li className={cName}>
+        <button onClick={onPrevClick} className={style.link}>
+          Prev
+        </button>
+      </li>
+    )
+  }
 
-//   return (
-//     <>
-//       <div className={style.bar}>
-//         <div>{renderTitle()}</div>
-//       </div>
-//       <div className={style.bar}>
-//         <ul className={style.list}>
-//           {renderPrevButton()}
-//           {renderNumberButtons()}
-//           {renderNextButton()}
-//         </ul>
-//       </div>
-//     </>
-//   )
-// }
+  return (
+    <>
+      <div className={style.bar}>
+        <div>{renderTitle()}</div>
+      </div>
+      <div className={style.bar}>
+        <ul className={style.list}>
+          {renderPrevButton()}
+          {renderNumberButtons()}
+          {renderNextButton()}
+        </ul>
+      </div>
+    </>
+  )
+}
 
-// export default Pagination
-
-export default (_: PaginationProps) => <div />
+export default Pagination
